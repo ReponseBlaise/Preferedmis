284729044f5c73ba9c6d0a9f306b1c3a8c2f42d3
Blaise Mushimiyumukiza <mushimiyumukizab@gmail.com>
2026-03-09 14:35:35 +0200

    h

--- backend/src/server.js
+++ backend/src/server.js
@@ -7,19 +7,9 @@ const pool = require('./config/database');
 
 const app = express();
 
-app.use((req, res, next) => {
-  const origin = req.headers.origin;
-  if (!origin || origin.endsWith('.vercel.app') || origin === 'http://localhost:5173') {
-    res.setHeader('Access-Control-Allow-Origin', origin || '*');
-    res.setHeader('Access-Control-Allow-Credentials', 'true');
-    res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type,Authorization');
-  }
-  if (req.method === 'OPTIONS') {
-    return res.sendStatus(200);
-  }
-  next();
-});
+app.use(cors());
+
+app.options('*', cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
