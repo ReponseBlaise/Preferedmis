d2589ef3bcb1caacc3c955d43fdb3bd9714a62dd
Blaise Mushimiyumukiza <mushimiyumukizab@gmail.com>
2026-03-09 10:38:19 +0200

    hh

--- backend/src/server.js
+++ backend/src/server.js
@@ -7,24 +7,19 @@ const pool = require('./config/database');
 
 const app = express();
 
-app.use(cors({
-  origin: function(origin, callback) {
-    const allowedOrigins = [
-      'https://preferedmisui.vercel.app',
-      'http://localhost:5173',
-      process.env.FRONTEND_URL
-    ].filter(Boolean);
-    
-    if (!origin || allowedOrigins.includes(origin) || origin.endsWith('.vercel.app')) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
-}));
+app.use((req, res, next) => {
+  const origin = req.headers.origin;
+  if (!origin || origin.endsWith('.vercel.app') || origin === 'http://localhost:5173') {
+    res.setHeader('Access-Control-Allow-Origin', origin || '*');
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type,Authorization');
+  }
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+  next();
+});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
