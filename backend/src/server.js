fb47d450e44a18b2b4581cb07306cea4aa5906c3
Blaise Mushimiyumukiza <mushimiyumukizab@gmail.com>
2026-03-16 00:22:37 +0200

    hhhh

--- backend/src/server.js
+++ backend/src/server.js
@@ -8,6 +8,7 @@ const app = express();
 
 const allowedOrigins = [
   process.env.FRONTEND_URL,
+  'https://mis.preferred.rw',
   'http://localhost:5173',
   'http://localhost:3000'
 ].filter(Boolean);
