e7ba2a55395a1e12561c01bb88666253d2810ef1
Blaise Mushimiyumukiza <mushimiyumukizab@gmail.com>
2026-03-28 20:35:28 +0200

    hyyfv

--- backend/src/server.js
+++ backend/src/server.js
@@ -29,7 +29,12 @@ app.use(cors({
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
 }));
-app.options('*', cors());
+app.use((req, res, next) => {
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+  next();
+});
 
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
